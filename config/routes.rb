e46fd1ef8b4a17aff818388938cfc2d737b72a2f
hvaldezrrc <hvaldez@rrc.ca>
2025-04-20 00:22:24 -0500

    Created navigation for user to navigate data at a high level

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :exercises, only: [:index]
+  resources :foods, only: [:index]
+
   get "home", to: "static_pages#home"
   get "about", to: "static_pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
